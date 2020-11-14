90248c7c241f5ff0f44dccaccc8ad60bd722e1a6
imakshat47 <imakshat@github.com>
2020-11-15 00:45:21 +0530

    v1 Add Firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAMzaU8uoEqHb86nWpIQPQOwn6E6pZr3jM",
-  authDomain: "tik-tok-clone-d80ec.firebaseapp.com",
-  databaseURL: "https://tik-tok-clone-d80ec.firebaseio.com",
-  projectId: "tik-tok-clone-d80ec",
-  storageBucket: "tik-tok-clone-d80ec.appspot.com",
-  messagingSenderId: "744552425317",
-  appId: "1:744552425317:web:3bfb428c027ea26205238c",
-  measurementId: "G-TQHT25TQN0",
+  <YOUR KEY>
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
